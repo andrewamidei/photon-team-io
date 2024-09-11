0b23fadb5b24b0c605a39e6df6b5b4b02a1cb506
Tyler Brandon <tb087@uark.edu>
2024-09-11 17:33:31 -0500

    Imported customtkinter and created window

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+# Andrew Amidei, Tyler Brandon, Doncan Conly, Evan Meyers, Pranav Polavarapu
+# Created 9/11/2024
 # Temporary main file for all code
 # Split code up before Sprint 2 turn-in
+
+# --- Import ---
+import customtkinter
+
+# --- Create Window ---
+app = customtkinter.CTk()			# creates customtkinter object called "app"
+app.geometry("800x800")				# sets pixel size of window to 800x800
+app.title("Photon Control Panel")	# sets titls of window to "Photon Control Panel"
+
+# --- Main Loop ---
+app.mainloop()
