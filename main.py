c5fa94da5e43a08d60ec4d6066cdf0ff8731fd8a
EvanMeyerss <evanpaulmeyers@gmail.com>
2024-09-11 14:24:57 -0500

    Added Rudimentary DB
     - Rudimentary DB added.
     - Will be updated once GUI is implemented

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 # Temporary main file for all code
 # Split code up before Sprint 2 turn-in
+
+
+# Database implementation
+#(Waiting on Andrew & Pranav to implement GUI & make semi-functional)
+--Create table
+CREATE TABLE player (
+  id INT,
+  codename VARCHAR(30)
+);
+
+--Place first record into table
+INSERT INTO player (id, codename)
+VALUES (1, 'EvanTest');
+
