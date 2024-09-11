887386589e8606e194f761c3ebbb3f1306623393
EvanMeyerss <evanpaulmeyers@gmail.com>
2024-09-11 14:30:35 -0500

    Separated Database
     - Placed player database in separate file with .sql filetype.

---
+++ playerDatabase.sql
@@ -0,0 +1,14 @@
+
+-- Database implementation
+--(Waiting on Andrew & Pranav to implement GUI & make semi-functional)
+-----------------------------------------------------------------------
+--Create table
+CREATE TABLE player (
+  id INT,
+  codename VARCHAR(30)
+);
+
+--Place first record into table
+INSERT INTO player (id, codename)
+VALUES (1, 'EvanTest');
+
