b3ee336cb8aa2d8b397703997a4c51c5fe4d9c44
Tyler Brandon <tb087@uark.edu>
2024-09-11 19:06:36 -0500

    Photon logo image now displays and removes itself Has not been tested in environment

--- main.py
+++ main.py
@@ -5,11 +5,30 @@
 
 # --- Import ---
 import customtkinter
+from PIL import Image
+import time
+
+# --- Global Definitions ---
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 800
+LOGO_WIDTH = 800
+LOGO_HEIGHT = 800
+SPLASH_SCREEN_DURATION = 5	# In seconds
 
 # --- Create Window ---
-app = customtkinter.CTk()			# creates customtkinter object called "app"
-app.geometry("800x800")				# sets pixel size of window to 800x800
+app = customtkinter.CTk()	# creates customtkinter object called "app"
+app.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT))	# sets pixel size of window to WINDOW_WIDTHxWINDOW_HEIGHT
 app.title("Photon Control Panel")	# sets titls of window to "Photon Control Panel"
 
-# --- Main Loop ---
-app.mainloop()
+# --- Display Splash Screen ---
+logo_image = customtkinter.CTkImage(dark_image = Image.open("Images/logo.jpg"), size = (LOGO_WIDTH, LOGO_HEIGHT))	# Creates CTkImage object called "logo_image" and adds the logo.jpg to it with size 800x800
+logo_image_label = customtkinter.CTkLabel(master = app, image = logo_image, text = '')	# Displays image "logo_image" onto window "app"
+logo_image_label.grid(column = 0, row = 0)	# Moves image to 0,0
+
+app.update()	# Updates window
+
+time.sleep(SPLASH_SCREEN_DURATION)	# Program will sleep for given seconds to show splash screen
+
+logo_image_label.destroy()	# Removes the logo image label
+
+app.mainloop()	# Updates and stops execution of code?
