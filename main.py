cb611eaba812a968ce21f7a834436fb97778b053
Andrew Amidei <errcode101@gmail.com>
2024-09-13 20:32:36 -0500

    Cleaned up code and split all ui code into ui.py.

--- main.py
+++ main.py
@@ -1,83 +1,38 @@
 # Andrew Amidei, Tyler Brandon, Doncan Conly, Evan Meyers, Pranav Polavarapu
 # Created 9/11/2024
 # Temporary main file for all code
+# TODO: Listed below
 # Split code up before Sprint 2 turn-in
 
 # --- Import ---
 import customtkinter as ctk
-from PIL import Image
-import time
 import psycopg2
 
 # --- Modules ---
-from ui import PhotonGUI
+import ui as ui
 
 # --- Global Definitions ---
 WINDOW_WIDTH = 900	# In pixels
 WINDOW_HEIGHT = 550	# In pixels
-SPLASH_SCREEN_DURATION = 5	# In seconds
-SPLASH_SCREEN_LOCATION = "Images/logo.jpg"
-
-# --- Functions ---
-def create_image(app_window, image_location, image_width, image_height, image_x, image_y):
-	# Creates an image label object then displays image of size on window at location.
-	# Inputs: window - customtkinter window object to display image on
-	#		  image_location - file location of image to be displayed
-	#		  image_width - width in pixels of the image
-	#		  image_height - height in pixels of the image
-	#		  image_x - x coordinate (in pixels) where upper left corner of image will be displayed relative to (0,0). Positive is right
-	#		  image_y - y coordinate (in pixels) where upper left corner of image will be displayed relative to (0,0). Positive is down
-	# Outputs: label - CTkLabel object containing image and supporting info such as size and location
-
-	i = ctk.CTkImage(light_image = Image.open(image_location), size = (image_width, image_height))	# Creates CTkImage object called "i" containing image file at location and size tuple
-	label = ctk.CTkLabel(master = app_window, image = i, text = "")	# Creates a label object used to display the image in the given window
-	label.place(x = image_x, y = image_y)	# Moves the image to the x and y coordinates
-	# return label
-
-def create_splash_screen(app_window):
-	app_window.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT))	# sets pixel size of window to WINDOW_WIDTHxWINDOW_HEIGHT
-	app_window.title("Photon Control Panel")	# sets titls of window to "Photon Control Panel"
-
-	center_window(app) # center the window
-
-	# --- Display Splash Screen ---
-	# removed "label image =" since no return was used
-	create_image(app, SPLASH_SCREEN_LOCATION, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0)	# Create the splash screen
-
-	app_window.update()	# Updates window
-
-	time.sleep(SPLASH_SCREEN_DURATION)	# Program will sleep for given seconds to show splash screen
-
-	# No longer needed
-	# logo_image.destroy()	# Delete the splash screen
-
-	return app_window
-
-def center_window(window):
-	# Center any window
-    window.update_idletasks()
-    width = window.winfo_width()
-    height = window.winfo_height()
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
-    x = (screen_width - width) // 2
-    y = (screen_height - height) // 2
-    window.geometry(f"{width}x{height}+{x}+{y}")
 
 
 # --- Main function ---
 if __name__ == "__main__":
-	# --- Create Window ---
-
 	app = ctk.CTk()	# creates customtkinter object
 
-	app = create_splash_screen(app)
+	app = ui.create_splash_screen(app, WINDOW_WIDTH, WINDOW_HEIGHT) # create start image
 
+	'''
+	TODO: Fix this issue:
+	
+	Behavior: Splash screen will kill itself and entry window will replace it.
+	Expexted behavuior: window will transition to entry screen.
+	'''
 	app.destroy() # kills the window
 
-	app = PhotonGUI(WINDOW_WIDTH, WINDOW_HEIGHT) # Opens entry terminal
+	app = ui.PhotonGUI(WINDOW_WIDTH, WINDOW_HEIGHT) # Opens entry terminal
 
-	center_window(app) # center the window
+	ui.center_window(app) # center the window
 
 	app.mainloop() # pauses the code
 
--- ui.py
+++ ui.py
@@ -1,5 +1,15 @@
 import customtkinter as ctk
-import tkinter as tk
+from PIL import Image
+import time
+
+'''
+The intention of this file is to serve as the ui library for Photon
+Lazer Tag software.
+
+As of 9/13/2024 this module will run a simple splash screen and
+display a entry terminal for use in setting up your game of lazer
+tag.
+'''
  
 # Sets the appearance of the window
 # Supported modes : Light, Dark, System
@@ -9,47 +19,95 @@ ctk.set_appearance_mode("Dark")
 # Supported themes : green, dark-blue, blue    
 ctk.set_default_color_theme("dark-blue")    
 
-# Default dimensions of the window
-# appWidth, appHeight = 500, 500
+SPLASH_SCREEN_DURATION = 5	# In seconds
+SPLASH_SCREEN_LOCATION = "Images/logo.jpg"
+TITLE = "Photon Control Panel"
  
 # App Class
 class PhotonGUI(ctk.CTk):
     # The layout of the window will be written
     # in the init function itself
-    def __init__(self, appWidth, appHeight, *args, **kwargs):
+    def __init__(self, app_width, app_height, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.playerID = "ID"
+        self.player_id = "ID"
         self.codename = "Codename"
  
         # Sets the title of the window to "App"
-        self.title("Photon Lazer Tag Entry Terminal")
+        self.title(TITLE)
         
         # Sets the dimensions of the window
-        self.geometry(f"{appWidth}x{appHeight}")    
+        self.geometry(f"{app_width}x{app_height}")    
 
         # Displays the ID entry box
-        self.idEntry = ctk.CTkEntry(self, placeholder_text=self.playerID)
+        self.id_entry = ctk.CTkEntry(self, placeholder_text=self.player_id)
         # Positions element in a grid
-        self.idEntry.grid(row=0, column=1, columnspan=2, padx=20, pady=20, sticky="ew")
+        self.id_entry.grid(row=0, column=1, columnspan=2, padx=20, pady=20, sticky="ew")
 
         # Displays the Codename entry box              
-        self.codenameEntry = ctk.CTkEntry(self, placeholder_text=self.codename)
+        self.codename_entry = ctk.CTkEntry(self, placeholder_text=self.codename)
         # Positions element in a grid
-        self.codenameEntry.grid(row=0, column=3, columnspan=2, padx=20, pady=20, sticky="ew")
+        self.codename_entry.grid(row=0, column=3, columnspan=2, padx=20, pady=20, sticky="ew")
                                   
         # Generate Button
-        self.generateResultsButton = ctk.CTkButton(self, text="Submit", command=self.generateResults)
+        self.generate_results_button = ctk.CTkButton(self, text="Submit", command=self.generateResults)
         # Positions element in a grid
-        self.generateResultsButton.grid(row=2, column=1, columnspan=2, padx=20, pady=20, sticky="ew")
+        self.generate_results_button.grid(row=2, column=1, columnspan=2, padx=20, pady=20, sticky="ew")
  
  
     # This function is used to insert the data into the varibles below
     def generateResults(self):
-        self.playerID = self.idEntry.get()
-        self.codename = self.codenameEntry.get()
+        self.player_id = self.id_entry.get()
+        self.codename = self.codename_entry.get()
 
         # print ID and Codename to the console for debug
         # TODO: Remove when finished debugging
-        print(f'ID:  {self.playerID} Codename: {self.codename}')
+        print(f'ID:  {self.player_id} Codename: {self.codename}')
+
+
+# --- Functions ---
+def create_image(app_window, image_location, image_width, image_height, image_x, image_y):
+	# Creates an image label object then displays image of size on window at location.
+	# Inputs: window - customtkinter window object to display image on
+	#		  image_location - file location of image to be displayed
+	#		  image_width - width in pixels of the image
+	#		  image_height - height in pixels of the image
+	#		  image_x - x coordinate (in pixels) where upper left corner of image will be displayed relative to (0,0). Positive is right
+	#		  image_y - y coordinate (in pixels) where upper left corner of image will be displayed relative to (0,0). Positive is down
+	# Outputs: label - CTkLabel object containing image and supporting info such as size and location
+
+	image = ctk.CTkImage(light_image = Image.open(image_location), size = (image_width, image_height))	# Creates CTkImage object called "i" containing image file at location and size tuple
+	label = ctk.CTkLabel(master = app_window, image = image, text = "")	# Creates a label object used to display the image in the given window
+	label.place(x = image_x, y = image_y)	# Moves the image to the x and y coordinates
+	# return label
+
+def create_splash_screen(app_window, window_width, window_height):
+	app_window.geometry(str(window_width) + "x" + str(window_height))	# sets pixel size of window to WINDOW_WIDTHxWINDOW_HEIGHT
+	app_window.title(TITLE)	# sets titls of window to "Photon Control Panel"
+
+	center_window(app_window) # center the window
+
+	# --- Display Splash Screen ---
+	# removed "label image =" since no return was used
+	create_image(app_window, SPLASH_SCREEN_LOCATION, window_width, window_height, 0, 0)	# Create the splash screen
+
+	app_window.update()	# Updates window
+
+	time.sleep(SPLASH_SCREEN_DURATION)	# Program will sleep for given seconds to show splash screen
+
+	# No longer needed
+	# logo_image.destroy()	# Delete the splash screen
+
+	return app_window
+
+def center_window(window):
+	# Center any window
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{x}+{y}")
 
