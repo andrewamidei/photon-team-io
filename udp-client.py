4f067414a7ce019b1a586242e09912ae347a76d0
Duncan Conly <97820614+DConly@users.noreply.github.com>
2024-09-18 13:20:26 -0500

    Update udp-client.py

--- udp-client.py
+++ udp-client.py
@@ -1,1 +1,17 @@
+import socket
 
+sentMsgClient = "Hello UDP Server"
+bytesToSend = str.encode(sentMsgClient)
+serverAddressPort = ("127.0.0.1", 7501)
+bufferSize = 1024
+
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
